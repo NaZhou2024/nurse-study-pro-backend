a7c1af62ed05c93cfac0f8b455a8dc45a4dcd086
NaZhou2024 <182044461+NaZhou2024@users.noreply.github.com>
2025-11-30 08:20:44 -0600

    Add corsMiddleware and fix cors block issue

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { loginUser } from "../controllers/authController.js";
+import { allowCors } from "../corsMiddleware.js";
 
 const router = express.Router();
 
 router.post("/login", loginUser);
 
-export default router;
+export default allowCors((req, res) => router(req, res));
