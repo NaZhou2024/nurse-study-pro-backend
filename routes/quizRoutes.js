3dd0b6a3a2d4a343774e2513343b2b2c8e8d8d60
NaZhou2024 <182044461+NaZhou2024@users.noreply.github.com>
2025-11-30 07:56:39 -0600

    fix quiz routes

--- routes/quizRoutes.js
+++ routes/quizRoutes.js
@@ -1,8 +1,16 @@
 import express from "express";
 import { getQuizzesByTopic } from "../controllers/quizController.js";
+import Quiz from "../models/Quiz.js";
 
 const router = express.Router();
 
-router.get("/:topicId", getQuizzesByTopic);
+router.get("/", async (req, res) => {
+  try {
+    const quizzes = await Quiz.find();
+    res.json(quizzes);
+  } catch (err) {
+    res.status(500).json({ message: "Failed to get quizzes" });
+  }
+});
 
 export default router;
