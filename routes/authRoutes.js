0b5b1037c864e8a33253ccd9c6d50a1806dc36dd
NaZhou2024 <182044461+NaZhou2024@users.noreply.github.com>
2025-11-30 08:55:56 -0600

    delete corsMiddleware from authRoutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { loginUser } from "../controllers/authController.js";
-import { allowCors } from "../corsMiddleware.js";
+
 
 const router = express.Router();
 
 router.post("/login", loginUser);
 
-export default allowCors((req, res) => router(req, res));
+export default router;
